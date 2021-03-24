2728728113d0202fbe67a98c751ef04ad4c6ef3f
czpstory <czpstory@love.com>
2021-03-24 17:27:26 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,17 @@
-    //注意：没次调用$.get()或$.post()或$.ajax()的时候，
+    //注意：每次调用$.get()或$.post()或$.ajax()的时候，
     //会先调用这个函数$.ajaxPrefilter()
     //在这个函数中会拿到我们给Ajax提供的配置对象
     $.ajaxPrefilter(function(options) {
 
         //在发起真正的Ajax请求之前，统一拼接请求的跟路径
         options.url = 'http://ajax.frontend.itheima.net' + options.url;
-        console.log(options.url)
+        // console.log(options.url)
+
+        //统一为有权限的接口，设置headers 请求头
+        if (options.url.indexOf('/my') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+
     })
